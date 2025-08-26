3f0ba48e4a52c1325413cb4ec83f66b927d05a04
Eric Fuentes <efuentesgarcia07@gmail.com>
2025-08-26 22:10:05 +0200

    Nuevos

---
+++ 18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program
+{
+    // El "public Nombre" (como sea), si es el mismo nombre que la clase, al crearlo se ejecuta
+    class Aplicacion
+    {
+        private double seguidores = 1000;
+        private double seguidos = 230;
+        public string nombre;
+        public Aplicacion(string nm)
+        {
+            nombre = nm;
+            Console.WriteLine("Acabas de crear tu cuenta.");
+        }
+        public string ConseguirNombre()
+        {
+            return nombre;
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Como te llamas?:");
+        Aplicacion usuario = new Aplicacion(Console.ReadLine());
+        Console.WriteLine("Bienvenid@, " + usuario.ConseguirNombre());
+    }
+}
+
---
+++ 19/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+    // El "public Nombre" (como sea), si es el mismo nombre que la clase, al crearlo se ejecuta
+    class Aplicacion
+    {
+        private string nombre;
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Aplicacion a = new Aplicacion();
+        a.Nombre = "Eric";
+        Console.WriteLine(a.Nombre);
+    }
+}
+
